e86724dddb0f8b7e548788729544b501acddf2e2
midas <midasdev711@gmail.com>
2019-11-25 09:00:24 -0500

    fix issues

--- content_script.js
+++ content_script.js
@@ -1,22 +1,8 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  async function(request, sender, sendResponse) {
     if( request.message !== null) {
       console.log(request.message);
       // var percent = request.message
-
-      function load(url, callback) {
-        var xhr = new XMLHttpRequest();
-      
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState === 4) {
-            callback(xhr.response);
-          }
-        }
-      
-        xhr.open('GET', url, true);
-        
-        xhr.send('');
-      }
 
       var pagenationNumber = document.querySelectorAll('li[class="a-normal"]');
       var pageTotal = parseInt(pagenationNumber[pagenationNumber.length - 1].innerText);
@@ -29,20 +15,42 @@ chrome.runtime.onMessage.addListener(
 
       let reviewUrlList = [];
 
-      orderList.forEach((e, i) => {
+      orderList.forEach(async(e, i) => {
         let shippedStatus = e.getElementsByClassName('shipped-status')[0];
         if (shippedStatus && shippedStatus.innerText == 'Payment complete') {
           let orderId = e.querySelectorAll('td:nth-of-type(3) a')[0].innerText;
           let reviewUrl = `https://sellercentral.amazon.com/messaging/reviews?orderId=${orderId}&marketplaceId=${marketPlaceId}`;
           reviewUrlList.push(reviewUrl);
-          var myWindow = window.open(reviewUrl, "_blank", 'width=100px, height=100px');
-          var buttons = myWindow.document.querySelectorAll('div[class="ayb-reviews-button-container"] button');
-          if (buttons.length > 0) {
-            buttons[1].click();
-          }
-          myWindow.close();
         }
       });
+
+      function clickReview(reviewUrl) {
+        return new Promise((resolve, reject) => {
+          var myWindow = window.open(reviewUrl, "_blank", 'width=100px, height=100px');
+          if (myWindow) {
+            myWindow.onload = function(){
+              setTimeout(function(){ 
+                var buttons = myWindow.document.querySelectorAll('div[class="ayb-reviews-button-container"] button');
+                let delayClose = 0;
+                if (buttons.length > 1) {
+                  console.log("- find buttons - ", reviewUrl) 
+                  buttons[1].click();
+                  delayClose = 2000;
+                }
+                setTimeout(function() {
+                  myWindow.close();
+                  resolve(true);
+                }, delayClose);
+              }, 2000);
+            };
+          }
+        })
+      }
+      reviewUrlList.reduce( (promise, url) => {      
+        return promise.then(() => {
+          return clickReview(url);
+        });
+      }, Promise.resolve());
     }
   }
 );
