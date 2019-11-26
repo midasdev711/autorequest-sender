5764a451f4e6029566606485fce6544b1febf7ea
midas <midasdev711@gmail.com>
2019-11-26 06:39:42 -0500

    update for pagination and add checkbox

--- content_script.js
+++ content_script.js
@@ -2,28 +2,56 @@ chrome.runtime.onMessage.addListener(
   async function(request, sender, sendResponse) {
     if( request.message !== null) {
       console.log(request.message);
-      // var percent = request.message
+      var customerReturnChecked = request.message;
 
-      var pagenationNumber = document.querySelectorAll('li[class="a-normal"]');
-      var pageTotal = parseInt(pagenationNumber[pagenationNumber.length - 1].innerText);
+      var pageTotal = document.querySelectorAll('ul[class="a-pagination"] span li').length;
 
       var dateFrom = new Date(document.querySelectorAll('input[data-input="start"]')[0].value).getTime();
-      var dateTo = new Date(document.querySelectorAll('input[data-input="start"]')[1].value).getTime();
+      var dateTo = new Date(document.querySelectorAll('input[data-input="start"]')[1].value).getTime() + 86399000;
 
-      var orderList = document.querySelectorAll('table[id="orders-table"] tbody tr');
-      let marketPlaceId = document.getElementById('a-page').querySelector('img').src.split('/')[7].split(':')[0];
+      var pageUrlList = [];
 
-      let reviewUrlList = [];
+      for (let i = 1; i <= pageTotal; i ++) {
+        let tmp = `https://sellercentral.amazon.com/orders-v3?page=${i}&date-range=${dateFrom}-${dateTo}`;
+        pageUrlList.push(tmp);
+      }
 
-      orderList.forEach(async(e, i) => {
-        let shippedStatus = e.getElementsByClassName('shipped-status')[0];
-        if (shippedStatus && shippedStatus.innerText == 'Payment complete') {
-          let orderId = e.querySelectorAll('td:nth-of-type(3) a')[0].innerText;
-          let reviewUrl = `https://sellercentral.amazon.com/messaging/reviews?orderId=${orderId}&marketplaceId=${marketPlaceId}`;
-          reviewUrlList.push(reviewUrl);
-        }
-      });
+      var reviewUrlList = [];
+      var marketPlaceId = document.getElementById('a-page').querySelector('img').src.split('/')[7].split(':')[0];
 
+      function getOrderList(pageUrl) {
+        return new Promise((resolve, reject) => {
+          var myWindow = window.open(pageUrl, "_blank", 'width=100px, height=100px');
+          if (myWindow) {
+            myWindow.onload = () => {
+              setTimeout(() => { 
+                var orderList = myWindow.document.querySelectorAll('table[id="orders-table"] tbody tr');
+                orderList.forEach(async(e, i) => {
+                  let shippedStatus = e.getElementsByClassName('shipped-status')[0];
+                  if (shippedStatus && shippedStatus.innerText == 'Payment complete') {
+                    let orderId = e.querySelectorAll('td:nth-of-type(3) a')[0].innerText;
+                    let isCustomerReturn = e.querySelectorAll('div[class="order-status-column"] span[class="secondary-status refund-is-applied"]');
+                    if (customerReturnChecked) {
+                      let reviewUrl = `https://sellercentral.amazon.com/messaging/reviews?orderId=${orderId}&marketplaceId=${marketPlaceId}`;
+                      reviewUrlList.push(reviewUrl);
+                    } else {
+                      if (isCustomerReturn.length == 0) {
+                        let reviewUrl = `https://sellercentral.amazon.com/messaging/reviews?orderId=${orderId}&marketplaceId=${marketPlaceId}`;
+                        reviewUrlList.push(reviewUrl);
+                      }
+                    }
+                  }
+                });
+                setTimeout(function() {
+                  myWindow.close();
+                  resolve(true);
+                }, 1000);
+              }, 2000);
+            };
+          }
+        })
+      }
+      
       function clickReview(reviewUrl) {
         return new Promise((resolve, reject) => {
           var myWindow = window.open(reviewUrl, "_blank", 'width=100px, height=100px');
@@ -46,11 +74,18 @@ chrome.runtime.onMessage.addListener(
           }
         })
       }
-      reviewUrlList.reduce( (promise, url) => {      
+
+      pageUrlList.reduce((promise, url) => {      
         return promise.then(() => {
-          return clickReview(url);
+          return getOrderList(url);
         });
-      }, Promise.resolve());
+      }, Promise.resolve()).then(_ => {
+        reviewUrlList.reduce( (promise, url) => {      
+          return promise.then(() => {
+            return clickReview(url);
+          });
+        }, Promise.resolve());
+      });
     }
   }
 );
--- popup_script.js
+++ popup_script.js
@@ -1,12 +1,20 @@
- var percent = null;
- function popup() {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+var checked = false;
+function popup() {
+  chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     var activeTab = tabs[0];
-    percent = "amazon";
-    chrome.tabs.sendMessage(activeTab.id, {"message": percent});
-   });
+    chrome.tabs.sendMessage(activeTab.id, {"message": checked});
+  });
+}
+
+function checkCustomerReturn() {
+  if (document.getElementById('customer_return').checked) {
+    checked = true;
+  } else {
+    checked = false;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("save_btn").addEventListener("click", popup);
+  document.getElementById("customer_return").addEventListener("click", checkCustomerReturn);
 });
